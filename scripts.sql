168625aa1425a97a004fcf47fd3e4e8e1f91eb86
Ubaithullah Ijabathullah <45236083+ubaith@users.noreply.github.com>
2025-01-21 14:40:30 +0400

    Update and rename README.md to scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,26 +1,15 @@
-# Converting columns to row in postgresql
+--Converting columns to row in postgresql
 
 select 
-
 col1
-
 , col2
-
 , col3
-
 , col_name
-
 , value1
-
 , value2
-
 from table_name
-
 , LATERAL (values
-
 ('ABC', abc1, abc2)
-
 , ('BCD', bcd1, bcd2)
-
 ) AS vals(col_name, value1, value2)
 ;
